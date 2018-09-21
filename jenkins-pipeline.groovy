5eea5f9d59755731e9d2f73219288d1fc3b641f5
jjamoreira <37379784+jjamoreira@users.noreply.github.com>
2018-09-21 10:33:24 -0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,14 +1,14 @@
  node('maven') {
     stage('build') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'oc', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'phpapp', showBuildLogs: 'true')
     }
     stage('verify') {
         echo 'dummy verification....'
     }
     stage('deploy') {
         input 'Manual Approval'
-        openshiftDeploy(deploymentConfig: 'oc')
+        openshiftDeploy(deploymentConfig: 'phpapp')
     }
     stage('promoting to QA') {
        echo 'fake stage...'
